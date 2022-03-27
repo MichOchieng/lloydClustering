57237635c8cf66bb7f96fdea3bcbec1d17279708
Mich Ochieng <michochieng@gmail.com>
2022-03-27 15:07:21 -0700

    First commit

---
+++ clustering.py
@@ -0,0 +1,13 @@
+import re
+
+class clustering:
+
+    def __init__(self) -> None:
+        pass
+
+    def run(self):
+        print("Hi")
+
+if __name__ == "__main__":
+    program = clustering()
+    program.run()
