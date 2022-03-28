ffdfc2053dbf3b77bbaf9b9673708c5ba9558b72
Mich Ochieng <michochieng@gmail.com>
2022-03-27 18:24:20 -0700

    Added error handling

--- clustering.py
+++ clustering.py
@@ -1,12 +1,27 @@
 import re
+import sys
 
 class clustering:
 
+    K:           int
+    clusterSize: int
+
+    clusters = []
+
     def __init__(self) -> None:
-        pass
+        self.getData()
+
+    def getData(self):
+        try:
+            with open(sys.argv[1], mode = 'r', encoding = 'utf-8') as file:
+                lines = file.readlines()
+        except IndexError:
+            print("Oops! You forgot to input a file name!")
+        except FileNotFoundError:
+            print("Error opening file " + sys.argv[1])
 
     def run(self):
-        print("Hi")
+        pass
 
 if __name__ == "__main__":
     program = clustering()
