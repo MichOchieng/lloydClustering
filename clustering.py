109fce02cc453ed368faefdc1eca4a46b1b7ab86
Mich Ochieng <michochieng@gmail.com>
2022-03-30 14:29:30 -0700

    Additions

--- clustering.py
+++ clustering.py
@@ -1,4 +1,3 @@
-import re
 import sys
 
 class clustering:
@@ -7,7 +6,8 @@ class clustering:
     M: int
 
     clusters  = []
-    centroid0 = []
+    centroids = []
+    data      = [] 
 
     def __init__(self) -> None:
         self.getData()
@@ -16,16 +16,37 @@ class clustering:
         try:
             with open(sys.argv[1], mode = 'r', encoding = 'utf-8') as file:
                 lines   = file.readlines()
-                self.K  = lines[0][0]
-                self.M  = lines[0][2]
-                self.centroid0 = list(map(float,lines[1].split()))
+                self.K  = int(lines[0][0])
+                self.M  = int(lines[0][2])
+                # Could probably do this without for loops
+                for x in range(1,self.K+1):
+                    self.centroids.append(
+                            list(
+                                map(
+                                    float,lines[x].split()
+                                    )
+                                )
+                            )
+                for x in range(self.K+1,len(lines)):
+                    self.data.append(
+                            list(
+                                map(
+                                    float,lines[x].split()
+                                    )
+                                )
+                            )
+                print(self.centroids)
+                print(self.data)
         except IndexError:
             print("Oops! You forgot to input a file name!")
         except FileNotFoundError:
             print("Error opening file " + sys.argv[1])
 
-    def run(self):
+    def llyod(self):
         pass
+
+    def run(self):
+        self.llyod()
 
 if __name__ == "__main__":
     program = clustering()
