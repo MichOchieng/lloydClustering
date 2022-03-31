751ea6c2d8c86ee097ed6e941a01e2c873ee9de5
Mich Ochieng <michochieng@gmail.com>
2022-03-30 19:00:53 -0700

    Added function to get distance between points

--- clustering.py
+++ clustering.py
@@ -1,13 +1,16 @@
+import math
 import sys
+import numpy as np
 
 class clustering:
 
     K: int
     M: int
 
-    clusters  = []
-    centroids = []
-    data      = [] 
+    clusters   = {}
+    clusterKey = []
+    centroids  = []
+    data       = [] 
 
     def __init__(self) -> None:
         self.getData()
@@ -36,14 +39,45 @@ class clustering:
                                 )
                             )
                 print(self.centroids)
-                print(self.data)
         except IndexError:
             print("Oops! You forgot to input a file name!")
         except FileNotFoundError:
             print("Error opening file " + sys.argv[1])
 
     def llyod(self):
-        pass
+        '''
+        Centriods to clusters
+        - Assign each data point to a centroid based on shortest distance
+            - can use distionaries for assignment with centroid as the key
+                - Convert arr val into a string
+
+        Clusters to centroid
+        - Compute new centroids from the points in their cluster
+            - Find the median
+
+        if centroids aren't the same run it back
+        '''
+        # Centroid to cluster
+        clusters = {}
+        test = []
+        self.getDistance(self.centroids[0],self.data[0])
+
+    def getDistance(self,arr0,arr1) -> float:
+        x = np.array(arr0)
+        y = np.array(arr1)
+
+        # Point comparison
+        z = np.array([x[i] - y[i] for i in range(len(x))])
+        # Hypot
+        accumulator = 0
+        for val in z:
+            val = val * val
+            accumulator = accumulator + val
+        distance = math.sqrt(accumulator)
+        return distance
+
+        
+
 
     def run(self):
         self.llyod()
