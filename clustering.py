d007b31bd381092a5c4636ec459cc156409c662d
Mich Ochieng <michochieng@gmail.com>
2022-04-01 01:22:26 -0700

    Fixed initialization of distance variable in centroid to cluster step

--- clustering.py
+++ clustering.py
@@ -41,29 +41,48 @@ class clustering:
                             )
         except IndexError:
             print("Oops! You forgot to input a file name!")
+            sys.exit()
         except FileNotFoundError:
             print("Error opening file " + sys.argv[1])
+            sys.exit()
 
     def llyod(self):
         '''
-        Centriods to clusters
-        - Assign each data point to a centroid based on shortest distance
-            - can use distionaries for assignment with centroid as the key
-                - Convert arr val into a string
+            Centriods to clusters
+            - Assign each data point to a centroid based on shortest distance
+                - can use distionaries for assignment with centroid as the key
+                    - Convert arr val into a string
 
-        Clusters to centroid
-        - Compute new centroids from the points in their cluster
-            - Find the median
+            Clusters to centroid
+            - Compute new centroids from the points in their cluster
+                - Find the median
 
-        if centroids aren't the same run it back
+            if centroids aren't the same run it back
         '''
         # Centroid to cluster
-        tempCluster = self.centroids[:]
-        clusters = dict()
+        clusters    = dict()
+
+        # for point in self.data:
+        #     distance      = -1
+        #     prevClusterID = 0
+        #     for i,centroid in enumerate(self.centroids):
+        #         if distance == -1:
+        #             # init
+        #             distance = self.getDistance(centroid,point)
+        #             prevClusterID = i
+        #             clusters[i].append(point)
+        #         elif self.getDistance(centroid,point) < distance:
+        #             distance = self.getDistance(centroid,point)
+        #             # Remove from prev cluster
+        #             clusters[prevClusterID].remove(point)
+        #             # Add to new
+        #             clusters[i].append(point)
+        # print(clusters)
+        prevCent = self.centroids[:]
         for point in self.data:
-            distance = 0
+            distance = -1
             for centroid in self.centroids:
-                if distance == 0:
+                if distance == -1:
                     distance = self.getDistance(centroid,point)
                     try:
                         clusters[str(centroid)].append(point)
@@ -71,6 +90,8 @@ class clustering:
                         clusters[str(centroid)] = []
                         clusters[str(centroid)].append(point)
                 elif self.getDistance(centroid,point) < distance:
+                    # update distance
+                    distance = self.getDistance(centroid,point)
                     # Remove from previous cluster
                     for value in clusters.values():
                         if point in value:
@@ -81,21 +102,24 @@ class clustering:
                     except KeyError:
                         clusters[str(centroid)] = []
                         clusters[str(centroid)].append(point)
+
         # Cluster to centroid
-        change = False
         for i,centriod in enumerate(self.centroids):
             try:
                 self.centroids[i] = np.mean(clusters[str(centriod)],axis = 0)
             except KeyError:
                 pass
-        if (np.array(tempCluster) == np.array(self.centroids)).all():
+        if (np.array(prevCent) == np.array(self.centroids)).all():
+            # Clusters have been sorted
             print(np.round(self.centroids,3))
         else:
             self.llyod()  
-                    
-    def initCluster(self,arr):
+
+    def initCluster(self):
+        arr = []
         for i in range(self.M):
             arr.append([])
+        return arr
 
     def getDistance(self,arr0,arr1) -> float:
         x = np.array(arr0)
